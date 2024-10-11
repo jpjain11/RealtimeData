1c97af01d45f6e8f0ff6cadb303c3a4557c8621c
Shri <jpjain996573@gmail.com>
2024-10-11 13:45:09 +0530

    Add main file

--- main.py
+++ main.py
@@ -15,21 +15,47 @@ def get_data():
     res=res.json()
     res=res['results'][0]
 
-def format_data():
+def format_data(res):
     data={}
+    location=res['location']
+    data['first_name']=res['name']['first']
+    data['last_name']=res['name']['last']
+    data['gender']=res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
 
 def stream_data():
-    import requests
+    import json
+    from kafka import KafkaProducer
+    import time
+    import logging
 
-    res=requests.get("https://randomuser.me/api/")
-    res=res.json()
-    res=res['results'][0]
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    print(res)
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
 with DAG('user_automation',
          default_args=default_args,
-         schedule_interval='@daily',
+         schedule='@daily',
          catchup=False) as dag:
 
     streaming_task = PythonOperator(
